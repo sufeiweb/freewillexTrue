2e3de43051f8511d59fe3e89bdb742756e9f16ab
sufeiweb <sufei_web@163.com>
2017-07-20 22:19:14 +0800

    BTC充值页面修改

--- src/components/userHome/accountManagement.vue
+++ src/components/userHome/accountManagement.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="userIndex">
     <div class="userIndex-cash-header">
-    <div class="header-nav">
-      <a href="javascript:" class="header-nav-active" @click="management_style1()">银行卡管理</a>
-      <a href="javascript:" @click="management_style2()">比特币地址管理</a>
+      <div class="header-nav">
+        <a href="javascript:" class="header-nav-active" @click="management_style1()">银行卡管理</a>
+        <a href="javascript:" @click="management_style2()">比特币地址管理</a>
+      </div>
     </div>
-  </div>
     <div class="management-bank-box" v-show="management_style">
       <div class="management-bank bank">
         <div class="bank-logo">
@@ -29,14 +29,14 @@
           <div>
             <span>比特币地址</span>
             <p>
-              <input type="number" placeholder="比特比地址"/>
+              <input type="text" placeholder="比特比地址"/>
               <span class="cash-tishi display-none-tishi">最小提现金额为100</span>
             </p>
           </div>
           <div>
             <span>备注</span>
             <p>
-              <input type="number" placeholder="备注"/>
+              <input type="text" placeholder="备注"/>
               <span class="cash-tishi display-none-tishi">最小提现金额为100</span>
             </p>
           </div>
@@ -85,12 +85,12 @@
 <script>
   import $ from 'jquery';
   export default {
-      data() {
-          return {
-            management_style: true,
-            btcAddBtn:true
-          }
-      },
+    data() {
+      return {
+        management_style: true,
+        btcAddBtn: true
+      }
+    },
     created (){
 
     },
@@ -101,23 +101,23 @@
         });
       }
       {
-          $('.management-bank').hover(function () {
-            $('.close-bank').animate({opacity:1,zIndex:1}).css({})
-          },function () {
-            $('.close-bank').animate({opacity:0,zIndex:-1}).css({})
-          });
+        $('.management-bank').hover(function () {
+          $('.close-bank').animate({opacity: 1, zIndex: 1}).css({})
+        }, function () {
+          $('.close-bank').animate({opacity: 0, zIndex: -1}).css({})
+        });
 
       }
     },
     methods: {
       management_style1() {
-          this.management_style = true;
+        this.management_style = true;
       },
       management_style2() {
         this.management_style = false;
       },
       BTC_add(){
-          this.btcAddBtn = false;
+        this.btcAddBtn = false;
       }
     }
   }
@@ -147,14 +147,16 @@
   .header-nav .header-nav-active {
     border-bottom-color: #337ab7 !important;
   }
-  .management-bank-box{
+
+  .management-bank-box {
     padding: 2rem 1.5rem;
     display: flex;
     justify-content: flex-start;
     flex-wrap: wrap;
     align-items: center;
   }
-  .management-bank{
+
+  .management-bank {
     margin: 0 2.083rem;
     margin-bottom: 3rem;
     border: 1px dashed #878787;
@@ -165,7 +167,8 @@
     height: 10.167rem;
     cursor: pointer;
   }
-  .management-bank.add{
+
+  .management-bank.add {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -173,33 +176,40 @@
     font-size: 1.167rem;
     color: #01aaef;
   }
-  .management-bank.add span:nth-of-type(1){
+
+  .management-bank.add span:nth-of-type(1) {
     font-size: 2rem;
   }
-  .management-bank.add:hover{
+
+  .management-bank.add:hover {
     color: #3175af;
     border-color: #01aaef;
   }
-  .management-bank.bank{
+
+  .management-bank.bank {
     display: flex;
     justify-content: space-between;
     flex-direction: column;
   }
-  .management-bank.bank:hover{
+
+  .management-bank.bank:hover {
     border-color: #01aaef;
   }
-  .management-bank.bank>div{
+
+  .management-bank.bank > div {
     display: flex;
     justify-content: space-between;
     align-items: center;
   }
-  .management-bank.bank>div i{
-    color:#01aaef ;
+
+  .management-bank.bank > div i {
+    color: #01aaef;
     font-size: 2rem;
     opacity: 0;
     z-index: -1;
   }
-  .BTC-add-btn{
+
+  .BTC-add-btn {
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -214,10 +224,12 @@
     cursor: pointer;
     border: 1px dashed #878787;
   }
-  .BTC-add-btn:hover{
+
+  .BTC-add-btn:hover {
     border-color: #01aaef;
   }
-  .management-BTC{
+
+  .management-BTC {
     padding: 3rem 2rem;
   }
 
@@ -238,13 +250,13 @@
     font-size: 1.167rem;
   }
 
-  .from-box > div:nth-of-type(1) > p,.from-box > div:nth-of-type(2) > p:nth-of-type(1), .from-box > div:nth-of-type(3) > p, .from-box > div:nth-of-type(5) > p {
+  .from-box > div:nth-of-type(1) > p, .from-box > div:nth-of-type(2) > p:nth-of-type(1), .from-box > div:nth-of-type(3) > p, .from-box > div:nth-of-type(5) > p {
     width: 30rem;
     height: 5rem;
     margin-bottom: -1.5rem;
   }
 
-  .from-box > div:nth-of-type(2) > p > input, .from-box > div:nth-of-type(3) > p > input {
+  .from-box > div:nth-of-type(1) > p > input, .from-box > div:nth-of-type(2) > p > input, .from-box > div:nth-of-type(3) > p > input {
     width: 90%;
     padding: 0.667rem 1rem;
   }
