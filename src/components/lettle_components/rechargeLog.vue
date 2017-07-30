71ee905eed26464c43ebca1a0c97bfd8bed2aa8d
sufeiweb <sufei_web@163.com>
2017-07-30 13:52:09 +0800

    充值页面重写，（账户页图标注释了）=》（修改）

---
+++ src/components/lettle_components/rechargeLog.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="userIndex-recharge-footer">
+    <el-tabs v-model="activeName" type="card">
+      <el-tab-pane label="充值记录" name="first">
+        <div class="recharge-record">
+          <table>
+            <thead>
+            <tr>
+              <td>充值时间</td>
+              <td>充值机构</td>
+              <td>充值金额</td>
+              <td>实充金额</td>
+              <td>状态</td>
+            </tr>
+            </thead>
+            <tbody class="recharge-record-data" v-show="!noRecord">
+            <tr>
+              <td>1</td>
+              <td>2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>5</td>
+            </tr>
+            </tbody>
+            <tbody class="recharge-record-data noRecode" v-show="noRecord">
+            <tr>
+              <td colspan="5">
+                <i class="iconfont">&#xe661;</i>
+                <span>暂无记录</span>
+              </td>
+            </tr>
+            </tbody>
+            <tfoot class="recharge-record-data-page">
+            <tr>
+              <td colspan="5">
+                <el-pagination layout="prev, pager, next" :total="10" class="page-right"></el-pagination>
+              </td>
+            </tr>
+            </tfoot>
+          </table>
+        </div>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+</template>
+<script>
+  export default {
+      data() {
+          return {
+            activeName:'first'
+          }
+      }
+  }
+</script>
+<style scoped>
+  .el-tabs__header{
+    margin-bottom: 0 !important;
+  }
+  .recharge-record{
+    padding: 0.833rem;
+    border:1px solid #d1dbe5;
+    border-top:none;
+    padding-top: 3rem;
+  }
+  .recharge-record table {
+    width: 100%;
+    border:1px solid #d1dbe5;
+    border-spacing: 0;
+    border-collapse: collapse;
+  }
+  .recharge-record table td{
+    width: 12.667rem;
+    padding: 0.883rem;
+    text-align: left;
+  }
+  .recharge-record table td:nth-of-type(1){width: 24.25rem}
+  .recharge-record table td:nth-of-type(2){width: 13rem}
+  .recharge-record table td:nth-of-type(3){width: 14.083rem}
+  .recharge-record table td:nth-of-type(4){
+    width: 14.167rem;
+  }
+  .recharge-record table td:nth-of-type(5){
+    width: 10rem;
+  }
+  .recharge-record thead{
+    border:1px solid #d1dbe5;
+  }
+  .recharge-record-data{
+    border: 1px solid #d1dbe5;
+    background: #f7f7f7;
+  }
+  .noRecode td{
+    padding: 6.667rem !important;
+    text-align: center !important;
+  }
+  .noRecode td i{
+    font-size: 3rem;
+  }
+  .noRecode td i,.noRecode td span{
+    display: inline !important;
+
+  }
+  .recharge-record-data-page td{
+    padding-top: 1.5rem;
+    text-align: right !important;
+  }
+
+</style>
--- src/components/lettle_components/userBtnLink.vue
+++ src/components/lettle_components/userBtnLink.vue
@@ -1,31 +1,40 @@
 <template>
   <div class="btcTransaction">
     <div class="btcTransaction-content">
-      <!--<ul class="btcTransaction-content-btn">-->
-        <!--<router-link to="/user" tag="li" class="li-active"><a href="javascript:"><i class="iconfont">&#xe639;</i>账户</a></router-link>-->
-        <!--&lt;!&ndash;<li class="li-active"><a href="javascript:"><i class="iconfont">&#xe639;</i>账户</a></li>&ndash;&gt;-->
-        <!--<li><a><i class="iconfont">&#xe69e;</i>资金管理</a></li>-->
-        <!--<router-link to="/recharge" tag="li"><a href="javascript:"><i class="iconfont">&#xe668;</i>充值</a></router-link>-->
-        <!--&lt;!&ndash;<li><a href="javascript:"><i class="iconfont">&#xe668;</i>充值</a></li>&ndash;&gt;-->
-        <!--<router-link to="/cash" tag="li"><a href="javascript:"><i class="iconfont">&#xe668;</i>提现</a></router-link>-->
-        <!--<router-link to="/accountDetails" tag="li"><a href="javascript:"><i class="iconfont">&#xe668;</i>账户明细</a></router-link>-->
-        <!--<router-link to="/accountManagement" tag="li"><a href="javascript:"><i class="iconfont">&#xe668;</i>账户管理</a></router-link>-->
-        <!--<router-link to="/reward" tag="li"><a href="javascript:"><i class="iconfont">&#xe687;</i>活动奖励</a></router-link>-->
-        <!--<router-link to="/loginLog" tag="li"><a href="javascript:"><i class="iconfont">&#xe6a0;</i>登录日志</a></router-link>-->
-        <!--<router-link to="/settings" tag="li"><a href="javascript:"><i class="iconfont">&#xe69b;</i>个人设置</a></router-link>-->
-        <!--<router-link to="/message" tag="li"><a href="javascript:"><i class="iconfont">&#xe69c;</i>消息管理</a></router-link>-->
-      <!--</ul>-->
       <dl class="btcTransaction-content-btn">
         <dt class="btcTransaction-content-title">我的账户</dt>
-        <router-link to="/userIndex" tag="dd"><a href="javascript:"><img src="../../assets/img/shiliang/cny.png" alt=""/>CNY账户</a></router-link>
-        <router-link to="/userIndex1" tag="dd"><a href="javascript:"><img src="../../assets/img/shiliang/btc.png" alt=""/>BTC账户</a></router-link>
+        <router-link to="/userIndex" tag="dd">
+          <a href="javascript:">
+            <!--<img src="../../assets/img/shiliang/cny.png" alt=""/>-->
+            <i class="iconfont">&#xe697;</i>
+            CNY账户
+          </a>
+        </router-link>
+        <router-link to="/userIndex1" tag="dd"><a href="javascript:">
+          <!--<img src="../../assets/img/shiliang/btc.png"  alt=""/>-->
+          <i class="iconfont">&#xe697;</i>
+          BTC账户
+</a>
+        </router-link>
       </dl>
       <dl class="btcTransaction-content-btn">
         <dt class="btcTransaction-content-title">资金管理</dt>
-        <router-link to="/recharge" tag="dd"><a href="javascript:"><img src="../../assets/img/shiliang/recharge.png" alt=""/>充值</a></router-link>
-        <router-link to="/cash" tag="dd"><a href="javascript:"><img src="../../assets/img/shiliang/cash.png" alt=""/>提现</a></router-link>
-        <router-link to="/accountDetails" tag="dd"><a href="javascript:"><i class="iconfont">&#xe697;</i>账户明细</a></router-link>
-        <router-link to="/accountManagement" tag="dd"><a href="javascript:"><i class="iconfont">&#xe69b;</i>账户管理</a></router-link>
+        <router-link to="/recharge" tag="dd"><a href="javascript:">
+          <!--<img src="../../assets/img/shiliang/recharge.png" alt=""/>-->
+          <i class="iconfont">&#xe697;</i>
+          充值
+</a>
+        </router-link>
+        <router-link to="/cash" tag="dd"><a href="javascript:">
+          <!--<img src="../../assets/img/shiliang/cash.png" alt=""/>-->
+          <i class="iconfont">&#xe697;</i>
+          提现
+</a>
+        </router-link>
+        <router-link to="/accountDetails" tag="dd"><a href="javascript:"><i class="iconfont">&#xe697;</i>账户明细</a>
+        </router-link>
+        <router-link to="/accountManagement" tag="dd"><a href="javascript:"><i class="iconfont">&#xe69b;</i>账户管理</a>
+        </router-link>
       </dl>
       <dl class="btcTransaction-content-btn">
         <dt class="btcTransaction-content-title">活动</dt>
@@ -33,8 +42,10 @@
       </dl>
       <dl class="btcTransaction-content-btn">
         <dt class="btcTransaction-content-title">安全中心</dt>
-        <router-link to="/settings" tag="dd"><a href="javascript:"><i class="iconfont">&#xe631;</i>安全设置</a></router-link>
-        <router-link to="/loginLog" tag="dd"><a href="javascript:"><i class="iconfont">&#xe6a0;</i>登录日志</a></router-link>
+        <router-link to="/settings" tag="dd"><a href="javascript:"><i class="iconfont">&#xe631;</i>安全设置</a>
+        </router-link>
+        <router-link to="/loginLog" tag="dd"><a href="javascript:"><i class="iconfont">&#xe6a0;</i>登录日志</a>
+        </router-link>
       </dl>
     </div>
   </div>
@@ -49,39 +60,42 @@
       });
       $('.btcTransaction-content-btn dd').hover(function () {
         $(this).children().css({
-          color:"#01aaef"
+          color: "#01aaef"
         });
         $(this).children().find('i').css({
-          color:"#01aaef"
+          color: "#01aaef"
         })
-      },function () {
+      }, function () {
         $(this).children().css({
-          color:"#333"
+          color: "#333"
         });
         $(this).children().find('i').css({
-          color:"#333"
+          color: "#333"
         })
       })
     }
   }
 </script>
 <style scoped>
-  .btcTransaction{
+  .btcTransaction {
     width: 18rem;
     background: #fafafc;
     height: 88rem;
     border-right: 1px solid #dbe1e9;
   }
-  .btcTransaction-content{
+
+  .btcTransaction-content {
     height: 100%;
   }
-  .btcTransaction-content-title{
+
+  .btcTransaction-content-title {
     color: #8e8e8e;
     font-size: 1.167rem;
     font-weight: 400;
     padding: 1.583rem 1.25rem 1.583rem 1.5rem;
   }
-  .btcTransaction-content-btn dd a{
+
+  .btcTransaction-content-btn dd a {
     display: flex;
     align-items: center;
     margin-top: 1px;
@@ -90,16 +104,19 @@
     font-size: 1.333rem;
     color: #555964;
   }
-  .btcTransaction-content-btn dd a i, .btcTransaction-content-btn dd a img{
+
+  .btcTransaction-content-btn dd a i, .btcTransaction-content-btn dd a img {
     margin-right: 1rem;
     font-size: 1.8rem;
     color: #555964
   }
-  .router-link-active>a,.router-link-active a>i{
+
+  .router-link-active > a, .router-link-active a > i {
     color: #fff !important;
     margin-right: -1px;
   }
-  .router-link-active{
+
+  .router-link-active {
     background: #01aaef;
     color: #fff !important;
   }
